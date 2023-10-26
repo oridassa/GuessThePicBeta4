58aaa22fdc1fc30690881b18158586c09b3184b5
oridassa <oridassa41@gmail.com>
2023-10-26 20:19:55 +0300

    commi1

--- GuessThePicBeta4/GameLobbyHost.cs
+++ GuessThePicBeta4/GameLobbyHost.cs
@@ -8,6 +8,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Security.Cryptography.X509Certificates;
+using Javax.Security.Auth;
+using Java.Lang;
+using System.Drawing;
+using System.ComponentModel;
+using System.Security.AccessControl;
+using Android.Animation;
 
 namespace GuessThePicBeta4
 {
@@ -40,11 +49,32 @@ namespace GuessThePicBeta4
             else if (b.Text == "Insert photos")
             {
                 //player pressed "Insert photos"
+                uploadpictures();
+
             }
             else if (b.Text == "Start Game")
             {
                 //host wants to start the game
+                intent = new Intent(this, typeof(GameplayScreen));
+                base.StartActivity(intent);
             }
+        }
+        private async void uploadpictures()
+        {
+            var image = await Xamarin.Essentials.MediaPicker.PickPhotoAsync();
+            Toast.MakeText(this, "entered function", ToastLength.Long).Show();
+            FirebaseClient firebase = new FirebaseClient(
+                    "https://guess-the-pic-a861a-default-rtdb.europe-west1.firebasedatabase.app/");
+            //await firebase
+            //    .Child("message")
+            //    .PutAsync<string>("hello");//changes the value of the child "message"
+
+            var result = await firebase
+                .Child("Games").PostAsync<string>("hello"); // code that posts the string "hello" in Games json with a random key **remind myself to check how to give it a custom key!!!!
+                
+            Toast.MakeText(this, "passed upload", ToastLength.Long).Show();
+            //string message = await firebase.Child("message").OnceSingleAsync<string>();
+            //Toast.MakeText(this, message, ToastLength.Long).Show();
         }
     }
 }
---
+++ GuessThePicBeta4/GameplayScreen.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GuessThePicBeta4
+{
+    [Activity(Label = "GameplayScreen")]
+    public class GameplayScreen : Activity
+    {
+        private TextView roundcounter;
+        private ImageView image;
+        private GridLayout grid;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.gameplay_screen);
+
+            this.roundcounter = FindViewById<TextView>(Resource.Id.roundcount);
+            this.image = FindViewById<ImageView>(Resource.Id.img);
+            this.grid = FindViewById<GridLayout>(Resource.Id.grid);
+
+            image.SetImageResource(Resource.Drawable.dog);
+            Button b1 = new Button(this);
+            b1.Text = "player 1";
+            b1.TextSize = 20;
+            b1.Click += OnClick;
+            grid.AddView(b1);
+
+
+        }
+
+        private void OnClick(object sender, EventArgs e)
+{
+            Toast.MakeText(this, "button worked", ToastLength.Long).Show();
+        }
+    }
+}
